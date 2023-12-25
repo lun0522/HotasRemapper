17f78b9f5fcef4e1aee5a2042442fe87d107b30b
Pujun Lun <lunpujun@gmail.com>
2023-12-24 17:24:09 -0800

    Create project.

---
+++ HotasRemapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotasRemapper
+//
+//  Created by Pujun Lun on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("Welcome to HOTAS Remapper!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ HotasRemapper/HotasRemapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotasRemapperApp.swift
+//  HotasRemapper
+//
+//  Created by Pujun Lun on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HotasRemapperApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
