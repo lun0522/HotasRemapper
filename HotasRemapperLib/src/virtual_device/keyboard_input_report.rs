d31ea4068b72af20819e48999ecc952eaf3c503a
Pujun Lun <lunpujun@gmail.com>
2024-01-29 23:06:05 -0800

    Update doc and code structure.

---
+++ HotasRemapperLib/src/virtual_device/keyboard_input_report.rs
@@ -0,0 +1,57 @@
+use std::ffi::c_char;
+
+enum ReportField {
+    _ModifierKeyStates = 0,
+    KeyStatesBegin = 1,
+    KeyStatesEndExclusive = 7,
+}
+
+const REPORT_LENGTH: usize = ReportField::KeyStatesEndExclusive as usize;
+
+pub(crate) struct KeyboardInputReport {
+    report: [c_char; REPORT_LENGTH],
+}
+
+impl KeyboardInputReport {
+    pub fn new() -> Self {
+        Self {
+            report: [0; REPORT_LENGTH],
+        }
+    }
+
+    pub fn report(&self) -> &[c_char; REPORT_LENGTH] {
+        &self.report
+    }
+
+    pub fn update_key_state(&mut self, key_code: c_char, is_pressed: bool) {
+        // If this key has been pressed previously, remove it from the report if
+        // it is now released.
+        if let Some(key_state) = self.find_key_state(key_code) {
+            if !is_pressed {
+                *key_state = 0x00;
+            }
+            return;
+        }
+        // If a new key is pressed, find an available slot for it.
+        if is_pressed {
+            match self.find_key_state(0x00) {
+                Some(key_state) => *key_state = key_code,
+                None => println!(
+                    "No available slot for key press (keycode: {})",
+                    key_code
+                ),
+            }
+        }
+    }
+
+    fn find_key_state(&mut self, key_code: c_char) -> Option<&mut c_char> {
+        for index in ReportField::KeyStatesBegin as usize
+            ..ReportField::KeyStatesEndExclusive as usize
+        {
+            if self.report[index] == key_code {
+                return Some(&mut self.report[index]);
+            }
+        }
+        None
+    }
+}
--- HotasRemapperLib/src/virtual_device/mod.rs
+++ HotasRemapperLib/src/virtual_device/mod.rs
@@ -1,5 +1,6 @@
 mod bluetooth_device;
 mod bluetooth_manager;
+mod keyboard_input_report;
 
 use std::ffi::c_char;
 use std::pin::Pin;
@@ -10,6 +11,7 @@ use anyhow::bail;
 use anyhow::Result;
 use bluetooth_device::DeviceInfo;
 use bluetooth_manager::SelectDevice;
+use keyboard_input_report::KeyboardInputReport;
 
 use crate::settings::VirtualDeviceSettings;
 use crate::ConnectionStatusCallback;
@@ -148,60 +150,5 @@ impl VirtualDevice {
         self.update_and_send_input_report(
             key_code, /* is_pressed= */ false,
         )
-    }
-}
-enum ReportField {
-    _ModifierKeyStates = 0,
-    KeyStatesBegin = 1,
-    KeyStatesEndExclusive = 7,
-}
-
-const REPORT_LENGTH: usize = ReportField::KeyStatesEndExclusive as usize;
-
-struct KeyboardInputReport {
-    report: [c_char; REPORT_LENGTH],
-}
-
-impl KeyboardInputReport {
-    pub fn new() -> Self {
-        Self {
-            report: [0; REPORT_LENGTH],
-        }
-    }
-
-    pub fn report(&self) -> &[c_char; REPORT_LENGTH] {
-        &self.report
-    }
-
-    pub fn update_key_state(&mut self, key_code: c_char, is_pressed: bool) {
-        // If this key has been pressed previously, remove it from the report if
-        // it is now released.
-        if let Some(key_state) = self.find_key_state(key_code) {
-            if !is_pressed {
-                *key_state = 0x00;
-            }
-            return;
-        }
-        // If a new key is pressed, find an available slot for it.
-        if is_pressed {
-            match self.find_key_state(0x00) {
-                Some(key_state) => *key_state = key_code,
-                None => println!(
-                    "No available slot for key press (keycode: {})",
-                    key_code
-                ),
-            }
-        }
-    }
-
-    fn find_key_state(&mut self, key_code: c_char) -> Option<&mut c_char> {
-        for index in ReportField::KeyStatesBegin as usize
-            ..ReportField::KeyStatesEndExclusive as usize
-        {
-            if self.report[index] == key_code {
-                return Some(&mut self.report[index]);
-            }
-        }
-        None
     }
 }
